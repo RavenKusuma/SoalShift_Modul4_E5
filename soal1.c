dfa8b661b8339cd8d4a6659e35945735d4c4b707
Unknown <rtejakusuma21@yahoo.com>
2017-11-15 04:12:00 +0700

    isi dari xmp_getattr

--- soal1.c
+++ soal1.c
@@ -10,7 +10,15 @@
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
+	int res;
+    char fpath[1000];
+    sprintf(fpath, "%s%s", dirpath, path);
+    res = lstat(fpath, stbuf);
 
+    if(res == -1)
+        return -errno;
+
+        return 0;
 }
 
 static int xmp_readdir(const char *path, void *buff, fuse_full_dir_t filler, off_t offset, struct fuse_file_info *fi)
@@ -23,7 +31,7 @@ static int xmp_read(const char *path, char *kata, size_t,off_t)
 
 }
 
-static int xmp_mkdir(const char *path,mode_t)
+static int xmp_mkdir(const char *path,mode_t mode)
 {
 
 }
