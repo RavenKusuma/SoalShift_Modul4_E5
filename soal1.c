8e2424a891c648c5cb4da5e3778a704a2da6dd87
Unknown <rtejakusuma21@yahoo.com>
2017-11-15 03:41:29 +0700

    tadi salah buat cuma rename filenya aja

---
+++ soal1.c
@@ -0,0 +1,21 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.read		= xmp_read,
+	.mkdir		= xmp_mkdir,
+	.write 		= xmp_write,
+};
+int main(int argc, char *argv[])
+{
+
+}
